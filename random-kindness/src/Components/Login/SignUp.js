7dfaab680ea9fac6fef531e4febb6148f36f21d4
Derek <legato_5@yahoo.com>
2019-10-17 15:50:03 -0700

    added fields with withFormik

--- random-kindness/src/Components/Login/SignUp.js
+++ random-kindness/src/Components/Login/SignUp.js
@@ -5,7 +5,13 @@ const SignUp = (props) => {
     return (
         <div className="sign-up-form">
             <Form>
-                <Field type='text' name='first' placeholder='First Name' />
+                <Field type="text" name="first" placeholder="First Name" />
+                <Field type="text" name="last" placeholder="Last Name" />
+                <Field type="text" name="email" placeholder="Email" />
+                <Field type="text" name="password" placeholder="********" />
+                <Field type="text" name="confirm" placeholder="********" />
+                <p>Already have an account? Log in now!</p>
+                <button type="submit">Sign Up</button>
             </Form>
         </div>
     )
