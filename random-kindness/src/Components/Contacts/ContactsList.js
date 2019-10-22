fbf4c0276d6e852548e4f7744d9a87915f913b67
Juan Aleman <juan.j.aleman@gmail.com>
2019-10-21 23:23:24 -0500

    installed axios and hooked up contacts page to display data

--- random-kindness/src/Components/Contacts/ContactsList.js
+++ random-kindness/src/Components/Contacts/ContactsList.js
@@ -1,54 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ContactForm from "./ContactsForm";
 import Contacts from "./Contacts";
-
-
+import axios from "axios";
 
 function ContactsApp() {
+  const [contacts, setContacts] = useState([]);
 
-    const [contacts, setContacts] = useState([
-        {
-            name: "Sample Dude",
-            phone: "1112223333",
-            email: "test@gmail.com",
-            address: "456 Street",
-            group: "friends",
-            notes: "loves help",
-            user_id: 1
-        }
-    ]);
+  useEffect(() => {
+    axios
+      .get("https://random-acts0519.herokuapp.com/api/contacts", { headers: {Authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0IjoxMDIsInVzZXJuYW1lIjoianRlc3QiLCJpYXQiOjE1NzE3MTYzNzYsImV4cCI6MTU3MTgwMjc3Nn0.is711-HYibx3pOUV0yzthL7VtG7j-hkT8Z3LFzvp1S4"} })
+      .then(response => {
+        const contactList = response.data;
+        console.log(response);
+        setContacts(contactList);
+      })
+      .catch(error => {
+        console.log("No Contacts", error);
+      });
+  }, []);
 
-    // Adding Contacts
-    const addContact = contact => {
-        const newContact = {
-            name: contact.name,
-            phone: contact.phone,
-            email: contact.email,
-            address: contact.address,
-            group: contact.group,
-            notes: contact.notes,
-            user_id: contact.user_id
-        };
-        setContacts([...contacts, newContact]);
+  // Adding Contacts
+  const addContact = contact => {
+    const newContact = {
+      name: contact.name,
+      phone: contact.phone,
+      email: contact.email,
+      address: contact.address,
+      group: contact.group,
+      notes: contact.notes,
+      user_id: contact.user_id
     };
+    setContacts([...contacts, newContact]);
+  };
 
-    // Deleting Contacts
-    const delContact = id => {
-        const newArr = contacts.filter(contact => {
-            return contact.user_id !== id;
-        });
-        setContacts(newArr);
-    };
+  // Deleting Contacts
+  const delContact = id => {
+    const newArr = contacts.filter(contact => {
+      return contact.user_id !== id;
+    });
+    setContacts(newArr);
+  };
 
-
-    return(
-        <div>
-            <h1>Contacts</h1>
-            <ContactForm  addContactFn={addContact}/>
-            <Contacts contactList={contacts} delContactFn={delContact}/>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Contacts</h1>
+      <ContactForm addContactFn={addContact} />
+      <Contacts contactList={contacts} delContactFn={delContact} />
+    </div>
+  );
 }
-
 
 export default ContactsApp;
