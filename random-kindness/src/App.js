f0fce5d7262b5e4d37f7f7a4ec149447bb01419f
Derek <legato_5@yahoo.com>
2019-10-22 18:10:54 -0700

    created forgot page with styling. need to make page functional still.

--- random-kindness/src/App.js
+++ random-kindness/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import SignUp from './Components/Login/SignUp.js';
 import LogIn from './Components/Login/LogIn.js';
 import Confirm from './Components/Login/Confirm.js';
+import Forgot from './Components/Login/Forgot.js';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/signup" component={SignUp} />
       <Route path="/login" component={LogIn} />
       <Route path="/signup-confirm" component={Confirm} />
+      <Route path="/forgot" component={Forgot} />
     </div>
   );
 }
