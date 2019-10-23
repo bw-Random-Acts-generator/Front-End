cf346794349509315edd9720e59a116bd9eec3ed
Derek <legato_5@yahoo.com>
2019-10-22 19:25:25 -0700

    added signup and login buttons

--- random-kindness/src/Components/Sign.js
+++ random-kindness/src/Components/Sign.js
@@ -4,8 +4,10 @@ import { Link } from 'react-router-dom';
 const Sign = () => {
     return (
         <div>
-            <button>Log In</button>
-            <button>Sign Up</button>
+            <Link to="/signup">Sign Up</Link>
+            <Link to="/login">Log In</Link>
         </div>
     )
 }
+
+export default Sign;
