c90818afbc762ca0c5b1066923997bba6295fca4
Juan Aleman <juan.j.aleman@gmail.com>
2019-10-24 22:58:01 -0500

    added styling to home page

--- random-kindness/src/Components/Home.js
+++ random-kindness/src/Components/Home.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import "./Home.css"
+
+
+var momPic = require("./mother.jpg")
+
+const homeBtn = {
+    backgroundColor: "#EB7F75",
+    width: "32rem",
+    height: "5.5rem",
+    borderRadius: "35px",
+    color: "white",
+    fontSize: "2rem",
+    cursor: "pointer",
+    textDecoration: "none",
+    padding: "1.5%",
+    boxShadow: "1px 4px 4px black",
+}
+
+
+
 const Home = (props) => {
     props.callBack('#ECE9E0');
     return (
-        <div>
+        <div className="containerHome">
+            <div className="picConStyle"><img className="picStyle" src={momPic}/></div>
+            <h1 className="home-title">Welcome!</h1>
+            <Link to={'/acts'} style={homeBtn}>GENERATE RANDOM ACT</Link>
         </div>
     )
 }
