c4964d74aec70133ea2486dbef48fb11a5996468
Derek <legato_5@yahoo.com>
2019-10-27 21:59:08 -0700

    don't merge files

---
+++ src/Components/RandomActs/Acts.js
@@ -0,0 +1,60 @@
+import React from "react";
+import axios from "axios";
+
+class Acts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: false,
+      acts: []
+    };
+  }
+  
+  componentDidMount = () => {
+    const token = localStorage.getItem("token");
+    const options = {
+      headers: {
+        Authorization: token
+      }
+    };
+
+    axios
+      .get("https://random-acts0519.herokuapp.com/api/actions", options)
+      .then(res => {
+        this.setState({
+          acts: res.data,
+          loggedIn: true
+        });
+      })
+      .catch(error => {
+        this.setState({
+          loggedIn: false
+        });
+      });
+  };
+
+  acts = () => {
+    if (this.state.loggedIn) {
+      return (
+        <div>
+          <h2>Acts</h2>
+          {this.state.acts.map(act => {
+            return (
+              <div key={act.id}>
+                <p>{act.description}</p>
+              </div>
+            );
+          })}
+        </div>
+      );
+    } else {
+      return <div>You need to be logged in!</div>;
+    }
+  };
+
+  render() {
+    return <this.acts />;
+  }
+}
+
+export default Acts;
